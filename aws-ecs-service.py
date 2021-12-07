7c90b211f9ced6733470b1b625faf58b7d0366aa
yyadav-tibco <yyadav@tibco.com>
2021-12-08 00:19:36 +0530

    test

--- aws-ecs-service.py
+++ aws-ecs-service.py
@@ -37,7 +37,7 @@ for each in list_services['serviceArns']:
     if service_nm in each:
         print("Deleting the exisitng service")
         delete_service = client.delete_service(cluster=cluster_nm,service=service_nm,force=True)
-        time.sleep(90)
+        time.sleep(120)
 
 
 
